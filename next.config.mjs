84ff0aed4e51aed94c0d38a81c6850c561398dac
Richa Gahlawat <148674336+Richa1509@users.noreply.github.com>
2025-03-11 23:33:17 +0530

    Add files via upload
    final changes

---
+++ next.config.mjs
@@ -0,0 +1,41 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '971edtce1a.execute-api.ap-south-1.amazonaws.com',
+      }
+    ],
+    domains: ['971edtce1a.execute-api.ap-south-1.amazonaws.com'],
+  },
+});
